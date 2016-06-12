09905ccfc075dc33659320150a96b0d8a9e7a077
Dimitri <dimitri.koshkin@gmail.com>
2016-06-12 10:24:08 -0400

    Enabled CORS

--- src/main/java/com/koshkin/demo/shape/service/controller/ShapeController.java
+++ src/main/java/com/koshkin/demo/shape/service/controller/ShapeController.java
@@ -8,10 +8,7 @@ import com.koshkin.demo.shape.service.exception.UnknownStateException;
 import com.koshkin.demo.shape.service.service.ShapeHttpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
@@ -20,6 +17,7 @@ import java.util.logging.Logger;
  */
 
 @RestController
+@CrossOrigin
 public class ShapeController {
 
     private static Logger log = Logger.getLogger(ShapeController.class.getName());
